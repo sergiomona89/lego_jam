aac1c4d7bad9f0a6afb8f6635fa430df3fda45a3
Sergio Andrés Moná Monsalve <sergio@localhost.localdomain>
2017-04-03 12:36:59 -0500

    se agregan las clases para poder usar las funcionalidades de BrickPi

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+#proyecto creado para competir en el Lego_JAM de la clase de Análisis y Diseño de Sistemas II
+
+import setuptools
+
+setuptools.setup(
+	name="lego_jam_12",
+	description="grupo 12 del lego Jam",
+	author="Sergio Andrés Moná, Jose Luis Aguirre",
+	py_modules=['BrickPi','ir_receiver_check'],
+)
